a154209e28c0ce3cb131bd6906815593d52c9b1b
EdsonJSF <edsonjsf1990@gmail.com>
2023-01-02 12:35:10 -0400

    Rutas principales

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+
 import { ErrorPageComponent } from './shared/error-page/error-page.component';
 
 @NgModule({
   declarations: [AppComponent, ErrorPageComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule,AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
 })
