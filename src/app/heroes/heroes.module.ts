8f4bb494a197e2e5728bfaa1fba8ddc09a2c3a10
EdsonJSF <edsonjsf1990@gmail.com>
2023-01-02 12:24:06 -0400

    modulos y componentes iniciales

---
+++ src/app/heroes/heroes.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AgregarComponent } from './pages/agregar/agregar.component';
+import { BuscarComponent } from './pages/buscar/buscar.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ListadoComponent } from './pages/listado/listado.component';
+
+@NgModule({
+  declarations: [
+    AgregarComponent,
+    BuscarComponent,
+    HomeComponent,
+    ListadoComponent,
+  ],
+  imports: [CommonModule],
+})
+export class HeroesModule {}
---
+++ src/app/heroes/pages/listado/listado.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-listado',
+  templateUrl: './listado.component.html',
+  styleUrls: ['./listado.component.scss'],
+})
+export class ListadoComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
