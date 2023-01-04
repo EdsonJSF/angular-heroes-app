7b75400d9d90284a235180b514134eb2e0e7895b
EdsonJSF <edsonjsf1990@gmail.com>
2023-01-03 21:34:08 -0400

    incorporacion del material navlist

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -3,12 +3,19 @@ import { CommonModule } from '@angular/common';
 
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
   declarations: [],
   imports: [CommonModule],
-  exports: [MatButtonModule, MatSidenavModule, MatToolbarModule, MatIconModule],
+  exports: [
+    MatButtonModule,
+    MatIconModule,
+    MatListModule,
+    MatSidenavModule,
+    MatToolbarModule,
+  ],
 })
 export class MaterialModule {}
