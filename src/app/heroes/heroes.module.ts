7df955b4924f69df5bc706e90877fbc2763fdbc9
Edsonjsf <edsonjsf1990@gmail.com>
2023-01-10 14:05:27 -0400

    Creacion del pipe para las imagenes

--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -12,6 +12,8 @@ import { HomeComponent } from './pages/home/home.component';
 import { ListadoComponent } from './pages/listado/listado.component';
 import { HeroeTarjetaComponent } from './components/heroe-tarjeta/heroe-tarjeta.component';
 
+import { ImagenPipe } from './pipes/imagen.pipe';
+
 @NgModule({
   declarations: [
     AgregarComponent,
@@ -20,6 +22,7 @@ import { HeroeTarjetaComponent } from './components/heroe-tarjeta/heroe-tarjeta.
     HomeComponent,
     ListadoComponent,
     HeroeTarjetaComponent,
+    ImagenPipe,
   ],
   imports: [
     CommonModule,
