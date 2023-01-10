840e4de87f30822a1dc81eda4c154cd990ebe63e
Edsonjsf <edsonjsf1990@gmail.com>
2023-01-10 13:50:09 -0400

    Creacion de HeroeTarjetaComponent

--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -10,6 +10,7 @@ import { BuscarComponent } from './pages/buscar/buscar.component';
 import { HeroeComponent } from './pages/heroe/heroe.component';
 import { HomeComponent } from './pages/home/home.component';
 import { ListadoComponent } from './pages/listado/listado.component';
+import { HeroeTarjetaComponent } from './components/heroe-tarjeta/heroe-tarjeta.component';
 
 @NgModule({
   declarations: [
@@ -18,6 +19,7 @@ import { ListadoComponent } from './pages/listado/listado.component';
     HeroeComponent,
     HomeComponent,
     ListadoComponent,
+    HeroeTarjetaComponent,
   ],
   imports: [
     CommonModule,
