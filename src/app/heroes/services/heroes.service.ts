2bf2a383a0441b075f5628c3fc1cbbd6d4936c46
Edsonjsf <edsonjsf1990@gmail.com>
2023-01-11 15:49:04 -0400

    Creando las variables de entorno

--- src/app/heroes/services/heroes.service.ts
+++ src/app/heroes/services/heroes.service.ts
@@ -2,19 +2,23 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
+import { environment } from 'src/environments/environment';
+
 import { Heroe } from '../interfaces/heroes.interface';
 
 @Injectable({
   providedIn: 'root',
 })
 export class HeroesService {
+  private baseURL = environment.baseURL;
+
   constructor(private http: HttpClient) {}
 
   getHeroes(): Observable<Heroe[]> {
-    return this.http.get<Heroe[]>(`http://127.0.0.1:3000/heroes`);
+    return this.http.get<Heroe[]>(`${this.baseURL}/heroes`);
   }
 
   getHeroeById(id: string): Observable<Heroe> {
-    return this.http.get<Heroe>(`http://127.0.0.1:3000/heroes/${id}`);
+    return this.http.get<Heroe>(`${this.baseURL}/heroes/${id}`);
   }
 }
