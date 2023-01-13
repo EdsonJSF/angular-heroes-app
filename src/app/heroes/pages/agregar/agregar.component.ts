ca6ce1fee6f95a500cc0d228b9659b60624cf5d3
Edsonjsf <edsonjsf1990@gmail.com>
2023-01-13 14:55:15 -0400

    Disenio de la pagina de agregar heroes

--- src/app/heroes/pages/agregar/agregar.component.ts
+++ src/app/heroes/pages/agregar/agregar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Heroe, Publisher } from '../../interfaces/heroes.interface';
 
 @Component({
   selector: 'app-agregar',
@@ -6,6 +7,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./agregar.component.scss'],
 })
 export class AgregarComponent implements OnInit {
+  publishers = [
+    {
+      id: 'DC Comics',
+      desc: 'DC - Comics',
+    },
+    {
+      id: 'Marvel Comics',
+      desc: 'Marvel - Comics',
+    },
+  ];
+
+  heroe: Heroe = {
+    superhero: '',
+    alter_ego: '',
+    characters: '',
+    first_appearance: '',
+    publisher: Publisher.DCComics,
+    alt_img: '',
+  };
+
   constructor() {}
 
   ngOnInit(): void {}
