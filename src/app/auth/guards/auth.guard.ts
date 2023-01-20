06948744bf79e57e5482e2de1388a33efb41ba25
Edsonjsf <edsonjsf1990@gmail.com>
2023-01-20 13:31:45 -0400

    implementacion del CanActivate en el guard y la ruta

--- src/app/auth/guards/auth.guard.ts
+++ src/app/auth/guards/auth.guard.ts
@@ -10,23 +10,29 @@ import {
 import { Observable } from 'rxjs';
 
 import { AuthService } from '../services/auth.service';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root',
 })
-export class AuthGuard implements CanLoad {
-  constructor(private authService: AuthService) {}
+export class AuthGuard implements CanLoad, CanActivate {
+  constructor(private authService: AuthService, private router: Router) {}
 
-  // canActivate(
-  //   route: ActivatedRouteSnapshot,
-  //   state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
-  //   return true;
-  // }
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): Observable<boolean> | Promise<boolean> | boolean {
+    return this.checkLogin();
+  }
 
   canLoad(
     route: Route,
     segments: UrlSegment[]
   ): Observable<boolean> | Promise<boolean> | boolean {
+    return this.checkLogin();
+  }
+
+  checkLogin(): boolean {
     if (this.authService.auth?.id) {
       return true;
     }
