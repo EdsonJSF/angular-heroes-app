698f8634dadc2777c39c03837c40d45c125e08cb
Edsonjsf <edsonjsf1990@gmail.com>
2023-01-20 14:24:34 -0400

    Cambiando el nombre de la aplicacion

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# AngularLazyload
+# LazyloadApp
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 14.1.0.
 
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular-lazyload'`, () => {
+  it(`should have as title 'lazyload-app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-lazyload');
+    expect(app.title).toEqual('lazyload-app');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('angular-lazyload app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('lazyload-app app is running!');
   });
 });
